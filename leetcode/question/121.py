1daa2bab94ce39712c4ed9baec4173d05f447815
Jagatees <140966272+Jagatees@users.noreply.github.com>
2025-05-24 01:13:55 +0800

    Done : 121

--- leetcode/question/121.py
+++ leetcode/question/121.py
@@ -4,3 +4,13 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        cheap_stock = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < cheap_stock:
+                cheap_stock = price
+            else:
+                profit = price - cheap_stock
+                max_profit = max(max_profit, profit)
+        return max_profit
