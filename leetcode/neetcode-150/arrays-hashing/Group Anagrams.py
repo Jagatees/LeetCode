4612a31e3b8799aeade9519335689f58e456b952
Jagatees <140966272+Jagatees@users.noreply.github.com>
2025-06-01 19:00:10 +0800

    dd

---
+++ leetcode/neetcode-150/arrays-hashing/Group Anagrams.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+
+
+
+sou = Solution()
+sou.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+
+        
--- leetcode/neetcode-150/arrays-hashing/Two Sum.py
+++ leetcode/neetcode-150/arrays-hashing/Two Sum.py
@@ -1,3 +1,19 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+                
+# Approach	Time Complexity	Space Complexity
+# Brute-force two loops	O(n²)	O(1)
+
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -5,10 +21,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        dict = {}
+        my_dict = {}
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in dict:
-                return [dict[diff], i]
-            dict[n] = i
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in my_dict and my_dict[comp] != i:
+                return [i, my_dict[comp]]
+            my_dict[nums[i]] = i
+
+            
+
+sou = Solution()
+print(sou.twoSum([3,3], 6))
