bebbdf5147e2b4863bdb83b401de9a6367a7603e
Jagatees <140966272+Jagatees@users.noreply.github.com>
2025-06-02 00:47:20 +0800

    Done : Group Anagrams

--- leetcode/neetcode-150/arrays-hashing/Group Anagrams.py
+++ leetcode/neetcode-150/arrays-hashing/Group Anagrams.py
@@ -4,11 +4,46 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        
+
+        sorted_strs = strs.copy()
+        for x in range(len(strs)):
+            sorted_strs[x] = "".join(sorted(strs[x]))
+
+        print(strs)
+        print(sorted_strs)
+        my_dict = {}
+
+        for x in range(len(sorted_strs)):
+            key = sorted_strs[x]
+            original_word = strs[x]
+
+            # Debug print: Show index and what we’re working on
+            print("Index:", x)
+            print("Sorted key:", key)
+            print("Original word:", original_word)
+
+            if key not in my_dict:
+                print(f"'{key}' not in dictionary, creating new list")
+                my_dict[key] = []
+            else:
+                print(f"'{key}' already in dictionary, appending to it")
+
+            print("Before Append Current dictionary:", my_dict)
+
+            my_dict[key].append(original_word)
+
+            # Show current state of the dictionary
+            print("Current dictionary:", my_dict)
+            print("------")
+
+        # Final result
+        print("Final grouped anagrams:", list(my_dict.values()))
 
 
 
-sou = Solution()
-sou.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
 
-        
+
+sol = Solution()
+sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+# sol.groupAnagrams([""])
+# sol.groupAnagrams(["a"])
--- leetcode/neetcode-150/arrays-hashing/Valid Anagram.py
+++ leetcode/neetcode-150/arrays-hashing/Valid Anagram.py
@@ -5,30 +5,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+
         if len(s) != len(t):
             return False
         
         my_dict = {}
         for x in s:
-            if x in my_dict:
-                my_dict[x] += 1
-            else:
-                my_dict[x] = 1
+            my_dict[x] = my_dict.get(x,0) + 1
 
         my_dict_2 = {}
         for x in t:
-            if x in my_dict_2:
-                my_dict_2[x] += 1
-            else:
-                my_dict_2[x] = 1
+            my_dict_2[x] = my_dict_2.get(x,0) + 1
 
-        print(my_dict)
-        print(my_dict_2)
 
         return my_dict == my_dict_2
-    
-
-
-# Example usage
-sol = Solution()
-print(sol.isAnagram("aacc", "ccac"))
